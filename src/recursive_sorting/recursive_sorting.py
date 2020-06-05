b01bb4466eb9d353447618d0e3f2aa1d6b063a88
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-04 20:31:14 -0700

    quicksort done

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -12,7 +12,11 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
+    # divide in half until subarrays len 1
+    # merge sorted lists together in groups of 2
+        # compare the first element of each group for smallest
+        # place smallest into array first 
+        # when one left, add to end of new array
 
     return arr
 
@@ -38,3 +42,35 @@ def timsort(arr):
     # Your code here
 
     return arr
+
+def partition(arr):
+    left = []
+    right = []
+    pivot = arr[0]
+
+    # iterate over the rest of the array
+    for num in arr[1:]:
+    # if the element is greater than pivot, in the right
+        if num > pivot:
+            right.append(num)
+    # else, in the left
+        else:
+            left.append(num)
+
+    return left, pivot, right
+
+
+
+def quick_sort(arr):  # O(N)
+    # choose a pivot ..can be first element
+    # move all elements LESS than the pivot to its LHS
+    # move all elements GREATER than the pivot to its RHS
+    if len(arr) == 0:
+        return arr
+
+    # helper function to find the pivot
+    # partition here into left, pivot, right
+    left, pivot, right = partition(arr)
+
+    return quick_sort(left) + pivot + quick_sort(right)
+    
