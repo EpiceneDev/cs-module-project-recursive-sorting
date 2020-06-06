8e0c173a3eac4d155478547c0fa93916eac4f76a
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-06 11:28:20 -0700

    bugged

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,12 +1,14 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements # sets space
+    merged_arr = []   #[0] * elements # sets space
     
    
     # compare and merge
     a = 0  # index of arrA (pointer A)
     b = 0  # index of arrB (pointer B)
+
+    # while a and b are not at the end of the array:
     while a < len(arrA) & b < len(arrB):
         if arrA[a] > arrB[b]:
             merged_arr.append(arrB[b])
@@ -15,29 +17,68 @@ def merge(arrA, arrB):
             merged_arr.append(arrA[i])
             a += 1
 
-        if arrA[a] <= arrB[b]:
-            merged_arr.append(arrA[i])
-            merged_arr.append(arrB[i])
-            i += 1
+    # if a reaches the end before b
+    while a < len(arrA):
+        merged_arr.append(arrA[a])
+        a+=1
 
+    # if b reaches the end before a
+    while b < len(arrB):
+        merged_arr.append(arrB[b])
+        b+=1
 
     return merged_arr
+
+'''
+from pair prog with Ari:
+def merge(left, right):
+    elements = len(left) + len(right)
+    merged_arr = [] #[0] * elements
+    # Your code here
+    # track index in merged_arr
+    r = 0
+    l = 0
+    # for i in range(elements):
+    #     # compare left-most of each arr
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            print(left,  right,  merged_arr, l, r)
+            merged_arr.append(left[l])
+            # iterate to next index in merged_arr
+            l+=1
+        if left[l] > right[r]:
+            print(left,  right,  merged_arr, l, r)
+            merged_arr.append(right[r])
+            r += 1
+        while r < len(right):
+            print(left,  right,  merged_arr, l, r)
+            merged_arr.append(right[r])
+            r+=1
+        while l < len(left):
+            print(left,  right,  merged_arr, l, r)
+            merged_arr.append(left[l])
+            l+=1
+    return merged_arr
+print(merge([2,4, 6, 8], [ 1, 3, 5, 7]))
+'''
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
+    # base case
+    if len(arr) <=1:
+        return arr
+        
     # divide in half until subarrays len 1
-    for i in range(len(arr)):
+    # for i in range(len(arr)):
    
-        while len(arr) > 1:
-            middle = len(arr)//2
-            right = arr[middle:]
-            left = arr[:middle]
-            merge_sort(right)
-            merge_sort(left)
-            
-        merge(right, left)
+    # while len(arr) > 1:
+    middle = len(arr)//2
+    right = merge_sort(arr[middle:])
+    left = merge_sort(arr[:middle])
+        
+    merge(right, left)
         # recursively call merge_sort() on LHS
         # recursively call merge_sort() on RHS
         # merge sorted pieces
